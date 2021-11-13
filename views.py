792ea2bac1ecda5ccf7668a09242d106ddb4e321
Alexey Khlybov <halexx7@gmail.com>
2021-11-13 20:58:10 +0000

    STEP-6 добавлено что-то

--- views.py
+++ views.py
@@ -1,14 +1,17 @@
 from datetime import date
 
-from components.models import Engine
+from components.models import Engine, MapperRegistry
 from components.decorators import AppRoute
 from components.cbv import ListView, CreateView
+from components.unit_of_work import UnitOfWork
 
 from origin.templator import render
 
 
 site = Engine()
 routes = {}
+UnitOfWork.new_current()
+UnitOfWork.get_current().set_mapper_registry(MapperRegistry)
 
 class NotFoud404:
     def __call__(self, request):
@@ -141,6 +144,10 @@ class StudentListView(ListView):
     queryset = site.students
     template_name = 'student_list.html'
 
+    def get_queryset(self):
+        mapper = MapperRegistry.get_current_mapper('student')
+        return mapper.all()
+
 
 # Класс-контроллер - Страница "Создать студента"
 @AppRoute(routes=routes, url='/create_student/')
@@ -152,6 +159,8 @@ class StudentCreateView(CreateView):
         name = site.decode_value(name)
         new_obj = site.create_user('student', name)
         site.students.append(new_obj)
+        new_obj.mark_new()
+        UnitOfWork.get_current().commit()
 
 
 # Класс-контроллер - Страница "Добавить студента на курс"
